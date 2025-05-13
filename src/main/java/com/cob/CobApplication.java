429e5aaaffd473c07b2582bcf7cd04b7f3adb6f5
Yuvaraj <yuvaraj.sf.java@gmail.com>
2025-05-13 14:15:27 +0530

    Initial spring boot setup

---
+++ src/main/java/com/cob/CobApplication.java
@@ -0,0 +1,13 @@
+package com.cob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cob/CobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
